3997ff665b20b9d115dde5b391a5742dc6aeaf0c
Kevin Jiao <kxjiao@gmail.com>
2019-07-29 10:18:21 -0400

    Remove seconds, change dataset to official dataset

--- taxi_data_process_query.sql
+++ taxi_data_process_query.sql
@@ -4,15 +4,12 @@ SELECT
   EXTRACT(DAYOFWEEK FROM trip_end_timestamp) AS day_of_week_end,
   (((EXTRACT(HOUR from trip_start_timestamp)*3600)+(EXTRACT(MINUTE from trip_start_timestamp)*60)+(EXTRACT(SECOND from trip_start_timestamp)))/86400) as start_time,
   (((EXTRACT(HOUR from trip_end_timestamp)*3600)+(EXTRACT(MINUTE from trip_end_timestamp)*60)+(EXTRACT(SECOND from trip_end_timestamp)))/86400) as end_time,
-  #EXTRACT(TIME FROM trip_start_timestamp) as start_time,
-  #EXTRACT(TIME FROM trip_end_timestamp) as end_time,
   EXTRACT(DAYOFYEAR FROM trip_start_timestamp) as start_day_of_year,
   EXTRACT(DAYOFYEAR FROM trip_end_timestamp) as end_day_of_year,
   EXTRACT(MONTH FROM trip_start_timestamp) as start_month,
   EXTRACT(MONTH FROM trip_end_timestamp) as end_month,
   EXTRACT(YEAR FROM trip_start_timestamp) as start_year,
   EXTRACT(YEAR FROM trip_end_timestamp) as end_year,
-  trip_seconds,
   trip_miles,
   pickup_community_area,
   dropoff_community_area,
@@ -21,7 +18,7 @@ SELECT
   dropoff_latitude,
   dropoff_longitude
 FROM
-  `ml-sandbox-1-191918.chicagotaxi.raw_taxidata`
+  `bigquery-public-data.chicago_taxi_trips.taxi_trips`
 WHERE
   trip_seconds > 0
   AND trip_miles > 0
