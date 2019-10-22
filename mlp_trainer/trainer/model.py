a9284d5624c8752ad15c24d028d440d0d928933b
Kendall Ocasio <kendallocasio@gmail.com>
2019-10-22 23:53:22 +0000

    Adjusting output

--- mlp_trainer/trainer/model.py
+++ mlp_trainer/trainer/model.py
@@ -203,7 +203,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
+    blob = bucket.blob(destination_blob_name.split(bucket_name + '/')[-1])
     blob.upload_from_filename(source_file_name)
 
 
