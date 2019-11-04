d9cca91fe5b29a83a6deec387c5988792421e96a
Zachary Mostowsky <zachary.mostowsky@ntconcepts.com>
2019-11-03 20:36:03 -0500

    Updates to tuning job.. write output to gcs

--- mlp_trainer/trainer/tune.py
+++ mlp_trainer/trainer/tune.py
@@ -14,7 +14,7 @@ def download_data_from_gcs(path):
     return df
 
 
-def tune(args):
+def tune(dataset_name, output_name):
     para = {
         'dense_neurons_1': [64, 9],
         'dense_neurons_2': [32],
@@ -30,32 +30,42 @@ def tune(args):
         'kernel_initial_3': ['normal']
     }
 
-    logging.info("Preprocessing Data")
-    print(args.filename)
-    X_train, y_train, X_test, y_test, X_val, y_val = model.process_data(args.filename)
+    logging.info("Preprocessing dataset {}.".format(dataset_name))
+    X_train, y_train, X_test, y_test, X_val, y_val = model.process_data(dataset_name)
 
-    logging.info
     # Run the tuning
+    logging.info('Running scan on hyper-parameters')
     scan_results = ta.Scan(x=X_train, y=y_train, x_val=X_val, y_val=y_val, params=para, model=model.train_mlp,
                            experiment_name='test_1')
 
     logging.info('Scanning complete.')
 
-    # Use Scan object as input
+    # Analyze the scan object
     analyze_object = ta.Analyze(scan_results)
 
-    # Access the DataFrame with the results
-    output_filename = 'hp_tuning.csv'
-    analyze_object.data.to_csv(output_filename)
+    # Save the DataFrame with the results
+    logging.info("Writing output to {}.".format(output_name))
+    analyze_object.data.to_csv(output_name)
+    # storage_client = storage.Client(project='ml-sandbox-1-191918')
+    # f = StringIO()
+    # analyze_object.data.to_csv(f)
+    # f.seek(0)
+    # storage_client.bucket('gcp-cert-demo-1').blob('hp_tune_test/hp_tuning.csv').upload_from_string(f.getvalue(), content_type='text/csv')
 
 
 def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        'filename',
+        'dataset_name',
         type=str,
         help='Dataset file local or GCS')
+
+    parser.add_argument(
+        'output_name',
+        type=str,
+        help='Output file to be saved to GCS')
+
     # parser.add_argument(
     #     '--test-split',
     #     type=float,
@@ -86,6 +96,15 @@ def get_args():
 
 
 if __name__ == '__main__':
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)-4.5s]  %(message)s',
+        handlers=[
+            logging.FileHandler('predictor.log'),
+            logging.StreamHandler()
+        ])
+
     args = get_args()
-    tune(args)
+    tune(args.dataset_name, args.output_name)
 
