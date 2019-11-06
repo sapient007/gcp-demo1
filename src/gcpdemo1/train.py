df44a59d78e38fd6e4ae6ec20c210c3af51feda8
Kendall Ocasio <kendall.ocasio@ntconcepts.com>
2019-11-05 21:51:10 -0500

    Tested starting job from script

--- src/gcpdemo1/train.py
+++ src/gcpdemo1/train.py
@@ -10,8 +10,8 @@ class MLPTrainer:
         self.project_name = project_name
         self.table_id = table_id
 
-    def train(self, table_id, dense_neurons_1, dense_neurons_2, dense_neurons_3, activation, dropout_rate_1,
-              dropout_rate_2, dropout_rate_3, optimizer, learning_rate, chunk_size, batch_size, epochs, validation_freq,
+    def train(self, dense_neurons_1, dense_neurons_2, dense_neurons_3, activation, dropout_rate_1, dropout_rate_2,
+              dropout_rate_3, optimizer, learning_rate, chunk_size, batch_size, epochs, validation_freq,
               kernel_initial_1, kernel_initial_2, kernel_initial_3, job_id=f'mlp_trainer_{time.time()}',
               job_dir=f'mlp_model_{time.time()}'):
 
@@ -21,14 +21,14 @@ class MLPTrainer:
         --scale-tier CUSTOM \
         --master-machine-type "standard_v100" \
         --staging-bucket "gs://gcp-cert-demo-1" \
-        --package-path "trainer" \
+        --package-path "../../mlp_trainer/trainer" \
         --module-name "trainer.task" \
         --job-dir "gs://gcp-cert-demo-1/{job_dir}" \
         --region "us-central1" \
         --runtime-version 1.5 \
         --python-version 3.5 \
         -- \
-        --table-id="{table_id}" \
+        --table-id="{self.table_id}" \
         --dense-neurons-1={dense_neurons_1} \
         --dense-neurons-2={dense_neurons_2} \
         --dense-neurons-3={dense_neurons_3} \
@@ -62,13 +62,26 @@ if __name__ == "__main__":
 
     mlp_trainer = MLPTrainer(
         project_name='ml-sandbox-1-191918',
-        table_id='finaltaxi_encoded_sampled_small')
+        table_id='finaltaxi_encoded_sampled_small'
+    )
 
     mlp_trainer.train(
-        table_id, dense_neurons_1, dense_neurons_2, dense_neurons_3, activation, dropout_rate_1,
-              dropout_rate_2, dropout_rate_3, optimizer, learning_rate, chunk_size, batch_size, epochs, validation_freq,
-              kernel_initial_1, kernel_initial_2, kernel_initial_3, job_id=f'mlp_trainer_{time.time()}',
-              job_dir=f'mlp_model_{time.time()}')
-
-    # linear_learner.deploy(model_prefix='demo1_linear_learner',
-    #                       version_prefix='version')
+        dense_neurons_1=64,
+        dense_neurons_2=32,
+        dense_neurons_3=8,
+        activation='relu',
+        dropout_rate_1=0.1,
+        dropout_rate_2=0.1,
+        dropout_rate_3=0.1,
+        optimizer='adam',
+        learning_rate=0.1,
+        chunk_size=500000,
+        batch_size=1024,
+        epochs=3,
+        validation_freq=5,
+        kernel_initial_1='normal',
+        kernel_initial_2='normal',
+        kernel_initial_3='normal',
+        job_id=f'mlp_trainer_src_test',
+        job_dir=f'mlp_model_src_test'
+    )
