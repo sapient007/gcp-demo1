d946a418f34bc356bcbbfd7fa087effa86c9184e
Erik Lincoln <erik.lincoln@ntconcepts.com>
2019-12-16 11:52:08 -0500

    kubeflow cleanup

--- mlp_trainer/setup.py
+++ mlp_trainer/setup.py
@@ -4,9 +4,12 @@ from setuptools import setup
 
 REQUIRED_PACKAGES = [
     'google-cloud-bigquery-storage[pandas,fastavro]==0.7.0',
-    'cloudml-hypertune==0.1.0.dev5',
-    'talos==0.6.3',
-    'google-cloud-storage==1.23.0'
+    'google-cloud-bigquery==1.22.0',
+    'google-cloud-storage==1.23.0',
+    'python-snappy==0.5.4',
+    'kfp==0.1.35',
+    'pytz>=2019.0,<2020'
+    'tfa-nightly'
 ]
 
 setup(
